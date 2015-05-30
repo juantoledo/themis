b84d907f111b5147a2c0020870b737272af09a9c
chantez <hector.poblete@usach.cl>
2015-05-29 23:32:48 -0300

    projecto con manejo sesion usando meteor y publicador de ley

---
+++ cabildo/client/home/ley/ley.js
@@ -0,0 +1,3 @@
+Template.ley.events({
+	
+});
---
+++ cabildo/client/home/ley/leyDetail.js
@@ -0,0 +1,5 @@
+Template.leyDetail.owner2 = function(){
+	var a1 = this;
+	//var a2 = this.params.query;
+	return Router.current().route.path(this);
+}
