357d880b55fbec9693321ecec065e3630b26f83b
chantez <hector.poblete@usach.cl>
2015-09-29 21:09:57 -0300

    separadas leyes de usuarios y congreso en home, y agregando senadores

--- cabildo/client/cabildo.js
+++ cabildo/client/cabildo.js
@@ -16,17 +16,48 @@ Template.moreCommentedLaws.rendered = function(){
 	})
 }
 
+Template.recentCongressLaws.rendered = function(){
+	Deps.autorun(function(){
+		Meteor.subscribe("laws");
+	})
+}
+
+Template.moreVotedCongressLaws.rendered = function(){
+	Deps.autorun(function(){
+		Meteor.subscribe("laws");
+	})
+}
+
+Template.moreCommentedCongressLaws.rendered = function(){
+	Deps.autorun(function(){
+		Meteor.subscribe("laws");
+	})
+}
+
 
 
 Template.recentLaws.recentLaws = function(){
-	return Laws.find({}, {sort:{date: -1}, limit:5}).fetch();
+	return Laws.find({"type": LAW_TYPE_USER}, {sort:{date: -1}, limit:5}).fetch();
 }
 
 Template.moreVotedLaws.moreVotedLaws = function(){
-	return Laws.find({}, { sort: [["votesQuantity", "desc"]], limit:5 });
+	return Laws.find({"type": LAW_TYPE_USER}, { sort: [["votesQuantity", "desc"]], limit:5 });
 }
 
 Template.moreCommentedLaws.moreCommentedLaws = function(){
 	
-	return Laws.find({}, { sort: [["commentsQuantity", "desc"]], limit:5 });
+	return Laws.find({"type": LAW_TYPE_USER}, { sort: [["commentsQuantity", "desc"]], limit:5 });
+}
+
+Template.recentCongressLaws.recentCongressLaws = function(){
+	return Laws.find({"type": LAW_TYPE_CONGRESS}, {sort:{date: -1}, limit:5}).fetch();
+}
+
+Template.moreVotedCongressLaws.moreVotedCongressLaws = function(){
+	return Laws.find({"type": LAW_TYPE_CONGRESS}, { sort: [["votesQuantity", "desc"]], limit:5 });
+}
+
+Template.moreCommentedCongressLaws.moreCommentedCongressLaws = function(){
+	
+	return Laws.find({"type": LAW_TYPE_CONGRESS}, { sort: [["commentsQuantity", "desc"]], limit:5 });
 }
